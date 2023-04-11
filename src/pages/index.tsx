15b1bf4789c2f69ead7d418adad12687042c2c99
WallaceRepo <sophiwebhub@gmail.com>
2023-04-11 14:45:21 -0500

    toast.js for error, posting on enter, posting show loading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,9 +9,10 @@ import type { RouterOutputs } from "~/utils/api";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
-import { LoadingPage } from "~/components/loading";
+import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { string } from "zod";
 import { useState } from "react";
+import {toast} from "react-hot-toast"
 
 dayjs.extend(relativeTime)
 
@@ -22,30 +23,55 @@ const CreatePostWizard = () => {
 
   const ctx = api.useContext();
 
-  const {mutate, isLoading: isPosting } = api.posts.create.useMutation({
+  const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
     onSuccess: () => {
       setInput("");
       void ctx.posts.getAll.invalidate();
     },
+    onError: (e)=> {
+      const errorMessage = e.data?.zodError?.fieldErrors.content;
+      if (errorMessage && errorMessage[0]) {
+        toast.error(errorMessage[0]);
+       } else {
+        toast.error("Failed to post! Please try again later.");
+      }
+    },
   })
   
   if (!user) return null;
+
   return (<div className="flex w-full gap-3">
     <Image src={user.profileImageUrl} 
           alt="Profile image" 
           className="w-14 h-14 rounded-full"
           width={56}
           height={56}/>
-    <input placeholder="Type some emojis!" 
-           className="grow bg-transparent outline-none"
-           value={input}
-           onChange={(e) => setInput(e.target.value)}
-           type="text"
-           disabled={isPosting}
-           />
-    <button onClick={()=> mutate({ content: input})}>Post</button>
-  </div>
-  )
+    <input
+        placeholder="Type some emojis!"
+        className="grow bg-transparent outline-none"
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            e.preventDefault();
+            if (input !== "") {
+              mutate({ content: input });
+            }
+          }
+        }}
+        disabled={isPosting}
+      />
+      {input !== "" && !isPosting && (
+        <button onClick={() => mutate({ content: input })}>Post</button>
+      )}
+      {isPosting && (
+        <div className="flex items-center justify-center">
+          <LoadingSpinner size={20} />
+        </div>
+      )}
+    </div>
+  );
 }
 type PostWithUser =  RouterOutputs["posts"]["getAll"][number];
 
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -64,16 +64,16 @@ export const postsRouter = createTRPCRouter({
    create: privateProcedure
     .input(
        z.object({
-         content: z.string().emoji().min(1).max(280),
+         content: z.string().emoji("Only emojis are allowed").min(1).max(280),
        })
    )
    .mutation(async ({ ctx, input }) => {
 
       const authorId = ctx.userId;
       
-      const {success} = await ratelimit.limit(authorId);
+      // const {success} = await ratelimit.limit(authorId);
 
-      if(!success ) throw new TRPCError({ code: "TOO_MANY_REQUESTS"});
+      // if(!success ) throw new TRPCError({ code: "TOO_MANY_REQUESTS"});
 
       const post = await ctx.prisma.post.create({
          data: {
