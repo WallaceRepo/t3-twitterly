fe3ee81b95324860dd3a5c34bde3ab895094bf9f
WallaceRepo <sophiwebhub@gmail.com>
2023-04-13 22:47:27 -0500

    fixed error with static page rendering with  Server Side Generator or SSG of tRPC

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,4 +1,4 @@
-import type { User } from "@clerk/nextjs/dist/api";
+
 import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
@@ -9,14 +9,8 @@ import { createTRPCRouter,
       } from "~/server/api/trpc";
 import { Ratelimit } from "@upstash/ratelimit";
 import {Redis } from "@upstash/redis"
+import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 
-const filterUserForClient = (user: User) => {
-      return {
-        id:user.id,
-        username: user.username,
-        profileImageUrl: user.profileImageUrl
-      }
-};
 // Create a new ratelimiter, that allows 3 requests per 1 minute
 const ratelimit = new Ratelimit({
    redis: Redis.fromEnv(),
